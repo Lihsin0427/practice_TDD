d7797c52875e833cf88e08c42505b7421f6578b7
“Lihsin0427” <lihsin0427@gmail.com>
2022-03-05 21:14:35 +0800

    搬進 context XXXXx do ...end

--- bank_spec.rb
+++ bank_spec.rb
@@ -2,27 +2,29 @@ require "./bank"
 
 RSpec.describe Bank do
   
-  # test case 測試個案
-  it "可以存錢"do
-  # 3A, Arrange / Act / Assert
+  context "存錢功能" do
 
-    account = Bank.new(10)
-    account.deposit(5)
+    # test case 測試個案
+    it "可以存錢"do
+    # 3A, Arrange / Act / Assert
 
-    expect(account.balance).to be 15
-  end
+      account = Bank.new(10)
+      account.deposit(5)
 
-  it "不可以存 0 元或是小於 0 元的金額" do
-    account = Bank.new(10)
-    begin 
-      account.deposit(-1)
-    rescue => e
-      p e
+      expect(account.balance).to be 15
     end
+
+    it "不可以存 0 元或是小於 0 元的金額" do
+      account = Bank.new(10)
       expect { account.deposit(-1) }.to raise_exception(BankError)
 
-    # expect(account.balance).to be 10
-    
+      # expect(account.balance).to be 10
+    end
+
   end
 
+  context "領錢功能" do
+
+  end
+  
 end
